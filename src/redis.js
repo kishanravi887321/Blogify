5856febc13c9a69744c701c592a8916938340849
Ravi Kishan <kishanravi887321@gmail.com>
2025-05-18 23:27:08 +0530

    added redis url

--- src/redis.js
+++ src/redis.js
@@ -1,5 +1,14 @@
 import {createClient} from "redis";
-const redisClient=createClient();
+import dotenv from "dotenv";
+dotenv.config({
+    path:"./.env"
+})
+
+const redis_url=process.env.REDIS_URL;
+console.log(redis_url)
+const redisClient=createClient({
+    url:redis_url
+});
 
 redisClient.on("error" ,(error)=>console.error("Redis error",error));
 
